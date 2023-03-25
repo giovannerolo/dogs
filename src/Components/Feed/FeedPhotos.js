82030a9dc672eaa442034f5150205fe2c33cd677
Giovanne <giovannerolo@outlook.com>
2023-03-25 02:41:51 -0300

    correcao no return map do Component Feed>FeedPhotos

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Loading from '../Helper/Loading';
 import { PHOTOS_GET } from '../../api';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
@@ -22,9 +22,13 @@ const FeedPhotos = () => {
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        {data.map((photo) => {
-          <FeedPhotoItem key={photo.id} photo={photo} />;
-        })}
+        {data.map((photo) => (
+          <FeedPhotoItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
       </ul>
     );
   else return null;
