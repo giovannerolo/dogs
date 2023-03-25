2434bc719e402ae006cbb57374aecd85714e722c
Giovanne <giovannerolo@outlook.com>
2023-03-25 02:38:12 -0300

    feed

---
+++ src/Components/Feed/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
+
+const Feed = () => {
+  return (
+    <section>
+      <FeedModal />
+      <FeedPhotos />
+    </section>
+  );
+};
+
+export default Feed;
