cf630503f1024e43775b43a7d81f7b013227180d
Giovanne <giovannerolo@outlook.com>
2023-03-28 14:25:55 -0300

    PageNotFound & PropTypes

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const NotFound = () => {
-  return <div className="container mainContainer">NotFound</div>;
+  return (
+    <div className="container mainContainer">
+      <h1 className="title">
+        Erro:404
+        <p>Page Not Found</p>
+      </h1>
+    </div>
+  );
 };
 
 export default NotFound;
